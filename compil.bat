b39487539b9b13ee0ab1a415d7d6f54624778858
mbonadeogh <108764840+mbonadeogh@users.noreply.github.com>
2024-05-12 00:20:38 -0300

    Add files via upload
    Por si quieres compilar los fuentes y generar nuevamente el jar, el archivo manifest.txt debe estar en el mismo directorio que compil.bat, quizás debas modificar el archivo compil.bat para indicar tu versión de java. En cualquier caso el jar provisto debería funcionar correctamente acorde a como se describe en el README.

---
+++ compil.bat
@@ -0,0 +1,2 @@
+javac com\mygames\batallanaval\BatallaNaval.java
+"C:\Program Files\Java\jdk-18.0.1\bin"\jar cfm BatallaNaval.jar manifest.txt com\mygames\batallanaval\*.class com\mygames\batallanaval\*.wav com\mygames\batallanaval\*.jpg 
